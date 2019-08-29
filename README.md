dd49cecab8915adb83f4211a84fcc2e4cc0f24eb
Yuci Gou <yuci.gou@gmail.com>
2019-08-29 23:13:48 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/29738381/how-to-publish-a-module-written-in-es6-to-npm
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let NewOneWithParameters = (a, b) => {
+  console.log(a + b); // 30
+};
+NewOneWithParameters(10, 20);
